829740ac87f02ce7f7df4e0e330658223bb01de3
Shruti Dhoot <shrutidhoot2001@gmail.com>
2024-11-23 22:37:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyAddPipe } from './my-add.pipe';
+import { MyMultPipe } from './my-mult.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyAddPipe, MyMultPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
